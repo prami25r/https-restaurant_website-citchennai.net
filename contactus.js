1c683c00320aa629fdfcae7ee5bf4a26a8403f2b
prami25r <115539120+prami25r@users.noreply.github.com>
2023-05-23 12:03:00 +0530

    Add files via upload

---
+++ contactus.js
@@ -0,0 +1,6 @@
+function submitForm(event) {
+    event.preventDefault();
+    // Add code to handle form submission here (e.g., send the data to a server or display a success message)
+    alert('Form submitted successfully!');
+  }
+  
